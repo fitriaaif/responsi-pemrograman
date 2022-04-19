2b9e650f661fe0681bc053652c4e4e018fce4d96
Fitria Amanda <101631102+fitriaaif@users.noreply.github.com>
2022-04-19 16:22:24 +0700

    gaji karyawan

---
+++ Karyawan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4272
+{
+    public class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajiBulanan { get; set; }
+        public int naikGaji { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gajiBulanan = 0;
+            }
+            else
+            {
+                this.gajiBulanan = gaji;
+            }
+
+            double tmp = 0;
+            tmp = 0.1 * gajiBulanan;
+            naikGaji = Convert.ToInt32(tmp) + gajiBulanan;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4272
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan(21114272, "Fitria", 2000000);
+            Karyawan karyawan2 = new Karyawan(21115964, "Amanda", 1500000);
+           
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("No \tNIK \t   \t Nama\t    \t\t Gaji");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1    {0} \t   \t {1} \t    \t {2}      ", karyawan.nik, karyawan.nama, karyawan.gajiBulanan);
+            Console.WriteLine("2    {0} \t   \t {1} \t    \t {2}      ", karyawan2.nik, karyawan2.nama, karyawan2.gajiBulanan);
+            Console.WriteLine("--------------------------------------------------------");
+
+            Console.WriteLine("\nAnda mendapatkan kenaikan gaji sebesar 10 %\n");
+
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("No \tNIK \t   \t Nama\t    \t\t Gaji");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("1    {0} \t   \t {1} \t    \t {2}      ", karyawan.nik, karyawan.nama, karyawan.naikGaji);
+            Console.WriteLine("2    {0} \t   \t {1} \t    \t {2}      ", karyawan2.nik, karyawan2.nama, karyawan2.naikGaji);
+            Console.WriteLine("--------------------------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
